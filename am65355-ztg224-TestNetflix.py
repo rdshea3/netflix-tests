8bc28519ceda4699dd7d4c16dee4c9097a8c4906
Alan Ma <alanma0907@utexas.edu>
2016-09-21 16:45:45 -0500

    test files added

---
+++ am65355-ztg224-TestNetflix.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.5
+
+# ---------------------------
+# Copyright (C) 2016
+# Alan Ma
+# Zachary Gilkerson
+# ---------------------------
+
+# -------
+# imports
+# -------
+
+from io import StringIO
+from unittest import main, TestCase
+
+from Collatz import netflix_read, netflix_predict, netflix_print, collatz_solve
+
+# -----------
+# TestNetflix
+# -----------
+
+
+class TestNetflix (TestCase):
+    # ----
+    # read
+    # ----
+    
+
+    # ----
+    # predict
+    # ----
+
+   
+    # -----
+    # print
+    # -----
+
+
+    # -----
+    # solve
+    # -----
+
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    main()
